28b57032dd40b6127f6e95a6e5c900caeff248f6
wojtur <mackowski28@gmail.com>
2024-10-15 19:07:27 +0200

    Initial commit

---
+++ src/main/java/org/example/playtogether/PlayTogetherApplication.java
@@ -0,0 +1,13 @@
+package org.example.playtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/playtogether/PlayTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.playtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
